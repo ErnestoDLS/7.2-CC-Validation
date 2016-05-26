ca9b62f0da3fa89a43fbbcdd303feea2c5f9f0ea
Ernesto De Los Santos <netos_id@yahoo.com>
2016-05-26 15:23:52 -0500

    need to finish it

---
+++ modules/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Validator from 'validator'
+
+export default React.createClass({
+  render() {
+    return (
+      <form>
+        <h1 className="heading">Hello!</h1>
+        <input type="text" 
+
+      </form>
+
+    )
+  }
+})
